369628562632699cb8053966ff2e4f028539ddc9
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2021-06-20 21:48:17 -0300

    Initial example

---
+++ example/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DockingExampleApp());
+}
+
+class DockingExampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Docking example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DockingExamplePage(),
+    );
+  }
+}
+
+class DockingExamplePage extends StatefulWidget {
+  @override
+  _DockingExamplePageState createState() => _DockingExamplePageState();
+}
+
+class _DockingExamplePageState extends State<DockingExamplePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center());
+  }
+}
