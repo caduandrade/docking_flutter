4a867c2120f8439aacd61f563e73aee8689113ad
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2021-08-14 18:21:40 -0300

    Moving demo to other repository

--- demo/lib/example_widget.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-abstract class ExampleStatelessWidget extends StatelessWidget {
-  Widget buildChild(int value) {
-    return Container(child: Center(child: Text('Child $value')));
-  }
-}
